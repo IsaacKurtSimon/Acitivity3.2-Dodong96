d5bea5135c1299079d463fbedffbaf26e7fb32c7
Dodong <22-36508@g.batstate-u.edu.phm>
2023-10-27 14:45:17 +0800

    feat/select-avg-playerlevel-per-class

---
+++ hero.sql
@@ -0,0 +1,6 @@
+SELECT c.class_name, AVG(p.player_level) as average_level
+FROM public.class c
+LEFT JOIN public.hero h ON c.class_id = h.class_id
+LEFT JOIN public.player p ON h.hero_id = p.hero_id
+GROUP BY c.class_name
+ORDER BY average_level DESC;
